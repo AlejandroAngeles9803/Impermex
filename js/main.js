c72bf8e32e5301765f8cc7ef6b2c2e64af2b4a6e
AlejandroAngeles9803 <112768600+AlejandroAngeles9803@users.noreply.github.com>
2023-06-02 19:03:33 -0600

    Changes in style and logig

--- js/main.js
+++ js/main.js
@@ -4,16 +4,11 @@ const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
 const avisoCookies = document.getElementById('aviso-cookies');
 const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
-avisoCookies.classList.add('activo');
-fondoAvisoCookies.classList.add('activo');
+
 
 botonAceptarCookies.addEventListener('click', () => {
-	avisoCookies.classList.remove('activo');
-	fondoAvisoCookies.classList.remove('activo');
-
-	localStorage.setItem('cookies-aceptadas', true);
-
-	dataLayer.push({'event': 'cookies-aceptadas'});
+    avisoCookies.classList.add('activo');
+    fondoAvisoCookies.classList.add('activo');
 });
 
 
