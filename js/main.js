1f9fd3921195585577336bc1757bb778f8d9f24c
AlejandroAngeles9803 <112768600+AlejandroAngeles9803@users.noreply.github.com>
2023-06-02 18:59:04 -0600

    JS modified

--- js/main.js
+++ js/main.js
@@ -4,14 +4,8 @@ const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
 const avisoCookies = document.getElementById('aviso-cookies');
 const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
-dataLayer = [];
-
-if(!localStorage.getItem('cookies-aceptadas')){
-	avisoCookies.classList.add('activo');
-	fondoAvisoCookies.classList.add('activo');
-} else {
-	dataLayer.push({'event': 'cookies-aceptadas'});
-}
+avisoCookies.classList.add('activo');
+fondoAvisoCookies.classList.add('activo');
 
 botonAceptarCookies.addEventListener('click', () => {
 	avisoCookies.classList.remove('activo');
